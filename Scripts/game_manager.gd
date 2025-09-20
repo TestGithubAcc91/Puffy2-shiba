0ef8a6847663272814e81deec45ab2add40f2805
TestGithubAcc91 <chiubrice@gmail.com>
2025-09-20 19:50:41 -0400

    coin counter

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,7 +1,14 @@
 extends Node
 
+@export var coin_counter_label: Label
+
 var score = 0 
 
 func add_point():
 	score += 1
+	update_label()
 	print("Score:" + str(score))
+
+func update_label():
+	if coin_counter_label:
+		coin_counter_label.text = "x" + str(score)
